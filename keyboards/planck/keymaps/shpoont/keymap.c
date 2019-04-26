f309f3e78596063daa4f817c8170f3e64b22ff7c
Leonid Komarovsky <leonid@komarovsky.info>
2019-04-26 19:29:53 +0300

    Switched keypad keys to regular ones

--- keyboards/planck/keymaps/shpoont/keymap.c
+++ keyboards/planck/keymaps/shpoont/keymap.c
@@ -129,10 +129,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, KC_P7, KC_P8,   KC_P9,   KC_PMNS,
-    _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_PGUP, KC_P4, KC_P5,   KC_P6,   KC_PPLS,
-    _______, _______, _______, _______, _______, KC_PIPE, KC_QUES, KC_PGDN, KC_P1, KC_P2,   KC_P3,   KC_PSLS,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, KC_PEQL, KC_PAST
+    KC_GRV,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, KC_7, KC_8,   KC_9,   KC_MINS,
+    _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_PGUP, KC_4, KC_5,   KC_6,   KC_PLUS,
+    _______, _______, _______, _______, _______, KC_PIPE, KC_QUES, KC_PGDN, KC_1, KC_2,   KC_3,   KC_SLSH,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_0, KC_DOT, KC_EQL, KC_ASTR
 ),
 
 /* Plover layer (http://opensteno.org)
